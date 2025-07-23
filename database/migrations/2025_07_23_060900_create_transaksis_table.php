57f851d745ba3697f3b39efcd344c2c60c719bc1
rakafebriansy <rakafebrian19@gmail.com>
2025-07-23 13:15:14 +0700

    init project

---
+++ database/migrations/2025_07_23_060900_create_transaksis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_transaksi', 20)->unique();
+            $table->date('tanggal');
+            $table->decimal('total_harga', 15, 2)->default(0);
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
