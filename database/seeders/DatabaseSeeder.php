64a065230017b3ba999251352de43698aa0668d4
rakafebriansy <rakafebrian19@gmail.com>
2025-07-28 13:36:44 +0700

    feat: seeder for transaksi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             KategoriSeeder::class,
             BarangSeeder::class,
+            TransaksiSeeder::class,
         ]);
     }
 }
